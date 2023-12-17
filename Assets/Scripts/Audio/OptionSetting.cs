a8af63807f29a7eecc27cafd6292b303958ccd9a
Daggoot <taksin_tang51@hotmail.com>
2023-12-18 02:43:54 +0700

    Resume game bugs

--- Assets/Scripts/Audio/OptionSetting.cs
+++ Assets/Scripts/Audio/OptionSetting.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
@@ -18,18 +17,9 @@ public class OptionSetting : MonoBehaviour
     private void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
+
         optionPanel.SetActive(false);
-
-        if (PlayerPrefs.HasKey("musicVolume"))
-        {
-            loadVolume();
-
-        }
-        else
-        {
-            setMusicVolume();
-            setSFXVolume();
-        }
+        LoadVolume();
     }
 
     private void Update()
@@ -37,41 +27,37 @@ public class OptionSetting : MonoBehaviour
         TogglePanel();
     }
 
-    public void setMusicVolume()
+    public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
-    public void setSFXVolume()
+    public void SetSFXVolume()
     {
         float volume = SFXSlider.value;
         myMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("sfxVolume", volume);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            //audioManager.PlaySFX(audioManager.PlaySFX()); 
-        }
-
     }
 
-    private void loadVolume()
+    private void LoadVolume()
     {
-        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
-        SFXSlider.value = PlayerPrefs.GetFloat("sfxVolume");
-
-        setMusicVolume();
-        setSFXVolume();
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+            SFXSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+            SetMusicVolume();
+            SetSFXVolume();
+        }
     }
 
     private void TogglePanel()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && optionPanel != null)
         {
             optionPanel.SetActive(!optionPanel.activeSelf);
-            Time.timeScale = optionPanel.activeSelf ? 0f : 1f; //// Pause and Resume the game
+            Time.timeScale = optionPanel.activeSelf ? 0f : 1f; // Pause and Resume the game
         }
     }
 
@@ -82,87 +68,60 @@ public class OptionSetting : MonoBehaviour
 
     private IEnumerator FadeAndLoadScene()
     {
-        fadeImage.gameObject.SetActive(true);
-
-        // Fade out
-        for (float alpha = 0f; alpha <= 1f; alpha += Time.deltaTime)
+        if (fadeImage != null)
         {
-            fadeImage.color = new Color(0f, 0f, 0f, alpha);
-            yield return null;
+            fadeImage.gameObject.SetActive(true);
+
+            // Fade out
+            for (float alpha = 0f; alpha <= 1f; alpha += Time.deltaTime)
+            {
+                fadeImage.color = new Color(0f, 0f, 0f, alpha);
+                yield return null;
+            }
+
+            // Load the next scene in the build index
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+
+            // Check if there is a next scene
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                Debug.LogWarning("No next scene available. Check Build Settings.");
+            }
         }
-
-        // Load the next scene in the build index
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        int nextSceneIndex = currentSceneIndex + 1;
-
-        // Check if there is a next scene
-        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
-        {
-            SceneManager.LoadScene(nextSceneIndex);
-        }
-        else
-        {
-            Debug.LogWarning("No next scene available. Check Build Settings.");
-        }
-
-        // // Fade in
-        // for (float alpha = 1f; alpha >= 0f; alpha -= Time.deltaTime)
-        // {
-        //     fadeImage.color = new Color(0f, 0f, 0f, alpha);
-        //     yield return null;
-        // }
-
-        // fadeImage.gameObject.SetActive(false);
     }
-
 
     public void ShowOption()
     {
-        optionPanel.SetActive(true);
-        Time.timeScale = 0f;
+        if (optionPanel != null)
+        {
+            optionPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
 
     public void HideOption()
     {
-        optionPanel.SetActive(false);
-        Time.timeScale = 1f;
+        if (optionPanel != null)
+        {
+            optionPanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Debug.LogError("optionPanel is not assigned!");
+        }
     }
 
     public void BackToMainMenu()
     {
-        StartCoroutine(FadeAndLoadMainMenu());
-    }
-
-    private IEnumerator FadeAndLoadMainMenu()
-    {
-        optionPanel.SetActive(false);
-        fadeImage.gameObject.SetActive(true);
+        // Set the time scale before loading the MainMenu scene
         Time.timeScale = 1f;
-
-        // Set the initial alpha value to 0 for the fade-in effect
-        fadeImage.color = new Color(0f, 0f, 0f, 0f);
-
-        // Fade in
-        for (float alpha = 0f; alpha <= 1f; alpha += Time.deltaTime)
-        {
-            fadeImage.color = new Color(0f, 0f, 0f, alpha);
-            yield return null;
-        }
-
-        // Load the MainMenu scene
         SceneManager.LoadScene("MainMenu");
-
-        // // Wait for a short duration before fading out
-        // yield return new WaitForSeconds(0.5f);
-
-        // // Fade out
-        // for (float alpha = 1f; alpha >= 0f; alpha -= Time.deltaTime)
-        // {
-        //     fadeImage.color = new Color(0f, 0f, 0f, alpha);
-        //     yield return null;
-        // }
-
-        // fadeImage.gameObject.SetActive(false);
     }
 
     public void ExitGame()
