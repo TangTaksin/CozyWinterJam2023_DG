74f5a922f336a0c50243f81a998280b0d95f2529
Daggoot <taksin_tang51@hotmail.com>
2023-12-18 01:41:40 +0700

    Update End Scene

--- Assets/EndTrigger.cs
+++ Assets/EndTrigger.cs
@@ -6,14 +6,33 @@ using UnityEngine.SceneManagement;
 public class EndTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject interactText;
+    bool canChangeScene = false;
     void Start()
     {
         interactText.SetActive(false);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        canChangeScene = true;
+        
+    }
     void OnTriggerStay(Collider other)
     {
+        canChangeScene = true;
         interactText.SetActive(true);
-        if (Input.GetKeyDown(KeyCode.E))
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        canChangeScene = false;
+        interactText.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canChangeScene == true)
         {
             SceneManager.LoadScene("EndScene");
         }
